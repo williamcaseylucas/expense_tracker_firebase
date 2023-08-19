72d2cacf3b841a8c6769ede9fcdeab59d82c416b
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2023-08-19 19:03:31 -0400

    finished expense tracker

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,23 +1,95 @@
 "use client";
+import {
+  addDoc,
+  collection,
+  // query,
+  // onSnapshot,
+  // querySnapshot,
+  doc,
+  getDocs,
+  deleteDoc,
+} from "firebase/firestore";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, MouseEventHandler } from "react";
+import { db } from "./firebase";
+
+type Items = {
+  id: string;
+  name: string;
+  price: number;
+};
 
 export default function Home() {
-  const [items, setItems] = useState([
-    { name: "Coffee", price: 4.95 },
-    { name: "Movie", price: 24.95 },
-    { name: "Candy", price: 7.95 },
+  const [items, setItems] = useState<Items[]>([
+    // { name: "Coffee", price: 4.95 },
+    // { name: "Movie", price: 24.95 },
+    // { name: "Candy", price: 7.95 },
   ]);
+  const [newItem, setNewItem] = useState<Items>({
+    name: "",
+    price: 0,
+    id: "0",
+  });
   const [total, setTotal] = useState(0);
 
   // Add items to db
-  const addItem = async (e) => {
+  const addItem = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    if (newItem.name !== "" && newItem.price !== 0) {
+      // Create new table by the name of items
+      const docRef = await addDoc(collection(db, "items"), {
+        name: newItem.name.trim(),
+        price: newItem.price,
+      });
+      setNewItem({ ...newItem, id: docRef.id }); // Add id to newItem
+      setItems([...items, newItem]); // Store it here
+      // console.log("newItem: ", newItem);
+      // console.log("docRef.id: ", docRef.id);
+    }
   };
 
   // Read items from db
+  useEffect(() => {
+    // const q = query(collection(db, "items"));
+    // const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    //   let itemsArr: Items[] = [];
+
+    //   // Have to push the values in this way
+    //   querySnapshot.forEach((doc) => {
+    //     itemsArr.push({ ...doc.data(), id: doc.id } as Items);
+    //   });
+    //   setItems(itemsArr);
+    //   console.log("items: ", items);
+    //   console.log("itemsArr: ", itemsArr);
+    // });
+
+    const readDB = async () => {
+      const querySnap = await getDocs(collection(db, "items"));
+      let itemsArr: Items[] = [];
+      querySnap.forEach((doc) => {
+        // console.log("doc.id: ", doc.id);
+        // console.log("doc.id: ", doc.data());
+        itemsArr.push({ ...doc.data(), id: doc.id } as Items);
+      });
+      setItems(itemsArr);
+
+      // Read total from itemsArr
+      const calculatedTotal = () => {
+        const totalPrice = itemsArr.reduce((sum, item) => sum + item.price, 0);
+        setTotal(totalPrice);
+      };
+
+      calculatedTotal();
+    };
+    readDB();
+  }, [items]);
 
   // Delete items from db
+  const deleteItem = async (id: string) => {
+    await deleteDoc(doc(db, "items", id));
+    setItems(items.filter((item) => item.id !== id)); // update the items by filtering out the clicked one
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
@@ -25,16 +97,23 @@ export default function Home() {
         <div className="bg-slate-800 p-4 rounded-lg">
           <form className="grid grid-cols-6 items-center text-black">
             <input
+              value={newItem.name}
+              onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
               className="col-span-3 p-3 border"
               type="text"
               placeholder="Enter Item"
             />
             <input
+              value={newItem.price}
+              onChange={(e) =>
+                setNewItem({ ...newItem, price: Number(e.target.value) })
+              }
               className="col-span-2 p-3 border mx-3"
               type="text"
               placeholder="Enter $"
             />
             <button
+              onClick={addItem}
               className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl"
               type="submit"
             >
@@ -51,7 +130,10 @@ export default function Home() {
                   <span className="capitalize">{item.name}</span>
                   <span>{item.price}</span>
                 </div>
-                <button className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">
+                <button
+                  onClick={() => deleteItem(item.id)}
+                  className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16"
+                >
                   X
                 </button>
               </li>
